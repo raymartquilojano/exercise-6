2ac81d8dd504108c1ef15836f6cf87a3676a316d
raymartquilojano <quilojanoraymart_bsit@plmun.edu.ph>
2024-10-02 12:11:35 +0800

    Add files via upload

---
+++ Copy of footer.php
@@ -0,0 +1,3 @@
+<hr color="black" size="4" noshade>
+<br>
+<center><small>https://github.com/GianPascua/Activity-6</small></center>
---
+++ Copy of header.php
@@ -0,0 +1,3 @@
+<center><font color="black">
+<h1>Welcome back!</h1></font>
+<hr color="black" size="4" noshade>
